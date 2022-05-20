b8d05daf9687bfd6e99cd33c46d84fab530b5363
NicolasSP12 <nicolas_salas_1200@hotmail.com>
2022-05-19 20:45:29 -0400

    Union de ambas partes y arreglos

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,23 @@
-//Url
-const URL = "https://pokeapi.co/api/v2/pokemon/"
-
-//Documento
+//variables
+const URL = "https://pokeapi.co/api/v2/pokemon/?limit=100"
 const d = document
-
-//Donde se imprimira
-const pokemonContainer = d.querySelector("#container_poke")
+const pokemonContainer = d.querySelector(".pokemons")
+const search = d.querySelector("#input_pokemon")
+let magicMonsters = []
 
 //Pintar la data
 const imprimirPokemons = (monster) => {
 
         const cardContainer = document.createElement("div")
-    
+        cardContainer.classList.add("imgystats")
+
         let cardBody = d.createElement("div")
         let name = d.createElement("p")
         let pokeID = d.createElement("p")
         let weight = d.createElement("p")
         let imgContainer = d.createElement("div")
         let imgPokemon = d.createElement("img")
+        imgContainer.classList.add("imgpoke")
         imgPokemon.setAttribute("src", monster.sprites.front_default)
 
         pokeID.innerText = `ID: ${monster.id}`
@@ -26,7 +26,7 @@ const imprimirPokemons = (monster) => {
         const listAbilities = abilities1or2(monster)
         const listTypes = types1or2(monster)
         
-        console.log(cardContainer)
+        // console.log(cardContainer)
 
         pokemonContainer.appendChild(cardContainer)
         cardContainer.appendChild(imgContainer)    
@@ -35,13 +35,24 @@ const imprimirPokemons = (monster) => {
         cardBody.append(pokeID, name, listTypes, weight, listAbilities)
 }
 
+// search.addEventListener("keyup", (event) => {
+//     pokemonContainer.innerHTML = ""
+//     busquedaPokemon(pokemones, event.target.value)
+// })
+
+// const busquedaPokemon = (Allmonster, pokemonSearch) => {
+//     let pokemonFound = Allmonster.filter((pokemon)=>{
+//         return pokemon.name.toLocaleLowerCase().includes(pokemonSearch.toLocaleLowerCase())
+//     })
+//     imprimirPokemons(pokemonFound)
+// }
+
 //Habilidades del pokemon
 const abilities1or2 = (monster) => {
     let abilities = d.createElement("ul")
-    abilities.innerText = `Especial ability(s):`
     monster.abilities.forEach(element => {
         let habilidad = d.createElement("li")
-        habilidad.innerText = element.ability.name
+        habilidad.innerText = `Especial ability(s):${element.ability.name}`
         abilities.appendChild(habilidad)
     });
     return abilities
@@ -50,10 +61,9 @@ const abilities1or2 = (monster) => {
 //Tipos del pokemon
 const types1or2 = (monster) => {
     let type = d.createElement("ul")
-    type.innerText = `Type(s):`
     monster.types.forEach(element => {
         let tipo = d.createElement("li")
-        tipo.innerText = element.type.name
+        tipo.innerText = `Type(s):${element.type.name}`
         type.appendChild(tipo)
     });
     return type
@@ -76,6 +86,7 @@ const pokeData = () => {
             const pokeInfo = poke.map(element => {
                 fetch(element)
                 .then(respuesta => respuesta.json())
+                // .then(pokemones => console.log(pokemones))
                 .then(pokemones => imprimirPokemons(pokemones))
             });
         })
